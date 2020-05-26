d3a7e3ae9d911369465c00534e1e01d4cce562f4
pnoel <pierreaxel.noel@gmail.com>
2020-05-26 20:53:03 +0200

    :construction_worker: adding ci

--- src/test/java/org/ApiManagerUser/ExampleResourceTest.java
+++ src/test/java/org/ApiManagerUser/ExampleResourceTest.java
@@ -1,6 +1,8 @@
 package org.ApiManagerUser;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,11 +12,16 @@ import static org.hamcrest.CoreMatchers.is;
 public class ExampleResourceTest {
 
     @Test
+    public void test() {
+        Assertions.assertEquals("", "");
+    }
+
+    @Test
     public void testHelloEndpoint() {
         given()
           .when().get("/hello")
           .then()
-             .statusCode(200)
+             .statusCode(500)
              .body(is("hello"));
     }
 
